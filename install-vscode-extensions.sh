2c6c73c1b96fe42c82f3864f35e5661e6b2426b8
kaspertofte <kaspertofte@gmail.com>
2026-04-07 10:17:46 +0200

    Use variable for VS Code CLI command in script

--- install-vscode-extensions.sh
+++ install-vscode-extensions.sh
@@ -4,6 +4,13 @@
 
 echo "Installing personal VS Code extensions..."
 
+CODE_CMD="${1:-${CODE_CLI:-code}}"
+
+if ! "$CODE_CMD" --version >/dev/null 2>&1; then
+    echo "VS Code CLI command '$CODE_CMD' is not usable, skipping extension installation"
+    exit 0
+fi
+
 # Array of extension IDs you want to install
 EXTENSIONS=(
     "github.vscode-pull-request-github" # https://github.com/Microsoft/vscode-pull-request-github
@@ -14,7 +21,7 @@ EXTENSIONS=(
 # Install each extension
 for ext in "${EXTENSIONS[@]}"; do
     echo "Installing $ext..."
-    code --install-extension "$ext" --force
+    "$CODE_CMD" --install-extension "$ext" --force
 done
 
 echo "Personal extensions installed!"
